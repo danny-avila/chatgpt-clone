b74cf6a6052ee6fc27112b57cca31fd3c8d7c840
Wentao Lyu <35-wentao.lyu@users.noreply.git.stereye.tech>
2023-03-14 00:55:26 +0800

    feat: add sample multi-user support feat: update README

--- api/models/Conversation.js
+++ api/models/Conversation.js
@@ -36,6 +36,9 @@ const convoSchema = mongoose.Schema({
   model: {
     type: String
   },
+  user: {
+    type: String
+  },
   suggestions: [{ type: String }],
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
 }, { timestamps: true });
@@ -43,9 +46,9 @@ const convoSchema = mongoose.Schema({
 const Conversation =
   mongoose.models.Conversation || mongoose.model('Conversation', convoSchema);
 
-const getConvo = async (conversationId) => {
+const getConvo = async (user, conversationId) => {
   try {
-    return await Conversation.findOne({ conversationId }).exec();
+    return await Conversation.findOne({ user, conversationId }).exec();
   } catch (error) {
     console.log(error);
     return { message: 'Error getting single conversation' };
@@ -53,16 +56,19 @@ const getConvo = async (conversationId) => {
 };
 
 module.exports = {
-  saveConvo: async ({ conversationId, title, ...convo }) => {
+  saveConvo: async (user, { conversationId, title, ...convo }) => {
     try {
       const messages = await getMessages({ conversationId });
       const update = { ...convo, messages };
       if (title) {
         update.title = title;
+        update.user = user
       }
 
+      console.error(user)
+
       return await Conversation.findOneAndUpdate(
-        { conversationId },
+        { conversationId: conversationId, user: user },
         { $set: update },
         { new: true, upsert: true }
       ).exec();
@@ -71,9 +77,9 @@ module.exports = {
       return { message: 'Error saving conversation' };
     }
   },
-  updateConvo: async ({ conversationId, ...update }) => {
+  updateConvo: async (user, { conversationId, ...update }) => {
     try {
-      return await Conversation.findOneAndUpdate({ conversationId }, update, {
+      return await Conversation.findOneAndUpdate({ conversationId: conversationId, user: user }, update, {
         new: true
       }).exec();
     } catch (error) {
@@ -81,13 +87,13 @@ module.exports = {
       return { message: 'Error updating conversation' };
     }
   },
-  // getConvos: async () => await Conversation.find({}).sort({ createdAt: -1 }).exec(),
-  getConvos: async (pageNumber = 1, pageSize = 12) => {
+  // getConvos: async () => await Conversation.find({}).sort({ created: -1 }).exec(),
+  getConvos: async (user, pageNumber = 1, pageSize = 12) => {
     try {
       const skip = (pageNumber - 1) * pageSize;
       // const limit = pageNumber * pageSize;
 
-      const conversations = await Conversation.find({})
+      const conversations = await Conversation.find({ user: user })
         .sort({ createdAt: -1 })
         .skip(skip)
         // .limit(limit)
@@ -101,17 +107,17 @@ module.exports = {
     }
   },
   getConvo,
-  getConvoTitle: async (conversationId) => {
+  getConvoTitle: async (user, conversationId) => {
     try {
-      const convo = await getConvo(conversationId);
+      const convo = await getConvo(user, conversationId);
       return convo.title;
     } catch (error) {
       console.log(error);
       return { message: 'Error getting conversation title' };
     }
   },
-  deleteConvos: async (filter) => {
-    let deleteCount = await Conversation.deleteMany(filter).exec();
+  deleteConvos: async (user, filter) => {
+    let deleteCount = await Conversation.deleteMany({...filter, user: user}).exec();
     deleteCount.messages = await deleteMessages(filter);
     return deleteCount;
   }
